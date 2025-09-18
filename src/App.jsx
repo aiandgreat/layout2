97ad1c9847be85311a3449ee772411f7cbab1392
aiandgreat <ajsgarcia.student@ua.edu.ph>
2025-09-18 17:40:23 +0800

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,41 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function BottomSection() {
+  return <div className="bottom-section">
+    <SideBoxes/>
+    <SmallCenterBoxes text="C-PCIT9"/>
+    <SmallCenterBoxes text="IT3A"/>
+    <SideBoxes/>
+  </div>
+}
+
+function SideBoxes () {
+    return <div className="box large-left purple"></div>
+}
+
+function SmallCenterBoxes (props) {
+    return <div className="box small-center peach">{props.text}</div>
+}
+
+function CenterBoxes() {
+  return <div className="center-boxes">
+    <TopBoxes name="Aian Jae"/>
+    <TopBoxes name="Garcia"/>
+  </div>;
+}
+
+function TopBoxes(props) {
+  return <div className="box-top pink">{props.name}</div>;
+}
+
 function App() {
 
   return (
     <div className="main-container">
         <div className="top-section">
             <div className="box peach"></div>
-            <div className="center-boxes">
-                <div className=" box-top pink"></div>
-                <div className=" box-top pink"></div>
-            </div>
+            <CenterBoxes/>
             <div className="box peach"></div>
         </div>
 
@@ -24,12 +49,7 @@ function App() {
             </div>
         </div>
 
-        <div className="bottom-section">
-            <div className="box large-left purple"></div>
-            <div className="box small-center peach"></div>
-            <div className="box small-center peach"></div>
-            <div className="box large-right purple"></div>
-        </div>
+        <BottomSection/>
     </div>
   )
 }
