c32c18df324ffe5a09c589dc8e7089e2634b6dc7
aiandgreat <ajsgarcia.student@ua.edu.ph>
2025-09-18 22:37:51 +0800

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,53 +3,41 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function BottomSection() {
-  return <div className="bottom-section">
-    <SideBoxes/>
-    <SmallCenterBoxes text="C-PCIT9"/>
-    <SmallCenterBoxes text="IT3A"/>
-    <SideBoxes/>
-  </div>
-}
-
-function SideBoxes () {
-    return <div className="box large-left purple"></div>
-}
-
-function SmallCenterBoxes (props) {
-    return <div className="box small-center peach">{props.text}</div>
-}
-
-function CenterBoxes() {
-  return <div className="center-boxes">
-    <TopBoxes name="Aian Jae"/>
-    <TopBoxes name="Garcia"/>
-  </div>;
-}
 
 function TopBoxes(props) {
   return <div className="box-top pink">{props.name}</div>;
+}
+function BottomBoxes(props) {
+  return <div className="box small-center peach">{props.text}</div>;
 }
 
 function App() {
 
   return (
-    <div className="main-container">
-        <div className="top-section">
-            <div className="box peach"></div>
-            <CenterBoxes/>
-            <div className="box peach"></div>
+    <div class="main-container">
+        <div class="top-section">
+            <div class="box peach"></div>
+            <div class="center-boxes">
+                <TopBoxes name="Aian Jae"/>
+                <TopBoxes name="Garcia"/>
+            </div>
+            <div class="box peach"></div>
         </div>
 
-        <div className="middle-section">
-            <div className="large-box lred"></div>
-            <div className="right-boxes">
-                <div className="lred"></div>
-                <div className="lred"></div>
+        <div class="middle-section">
+            <div class="large-box pink"></div>
+            <div class="right-boxes">
+                <div class="pink"></div>
+                <div class="pink"></div>
             </div>
         </div>
 
-        <BottomSection/>
+        <div class="bottom-section">
+            <div class="box large-left purple"></div>
+            <BottomBoxes text="C-PCIT9"/>
+            <BottomBoxes text="IT3A"/>
+            <div class="box large-right purple"></div>
+        </div>
     </div>
   )
 }
